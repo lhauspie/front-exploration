6ccf6e956579f554694cb4fea6083a70b8e4d492
Logan HAUSPIE <logan.hauspie.pro@gmail.com>
2020-04-26 12:39:55 +0200

    [React - Lab +] Try hooks

--- react-labs/client/src/LikeBtn.js
+++ react-labs/client/src/LikeBtn.js
@@ -5,29 +5,37 @@
  * - If type is "down", then a click on this button will decrement counter.
  */
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 const LikeBtn = ({ type, counter: initialCount }) => {
-  const [counter, setCounter] = useState(initialCount);
-  const increment = () => {
-    setCounter(prev => prev + 1);
-  };
-  const title = type === "up" ? "+1" : "-1";
-  return (
-    <button className="btn btn-default" title={title} onClick={increment}>
-      {counter} <i className={`glyphicon glyphicon-thumbs-${type}`}/>
-    </button>
-  );
+	const [counter, setCounter] = useState(initialCount);
+	const increment = () => {
+		setCounter(prev => prev + 1);
+	};
+	const title = type === "up" ? "+1" : "-1";
+
+	useEffect(
+		() => {
+			console.log("LikeBtn mounted");
+		},
+		[]
+	);
+
+	return (
+		<button className="btn btn-default" title={title} onClick={increment}>
+			{counter} <i className={`glyphicon glyphicon-thumbs-${type}`}/>
+		</button>
+	);
 };
 
 LikeBtn.defaultProps = {
-  counter: 0
+	counter: 0
 };
 
 LikeBtn.propTypes = {
-  type: PropTypes.oneOf(["up", "down"]).isRequired,
-  counter: PropTypes.number
+	type: PropTypes.oneOf(["up", "down"]).isRequired,
+	counter: PropTypes.number
 };
 
 export default LikeBtn;
